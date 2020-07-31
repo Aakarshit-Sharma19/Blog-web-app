493a5117d53d96f7fb19207ded4d0dcc5110a7da
Alabhya268 <singhalabhya32@gmail.com>
2020-08-01 04:45:34 +0530

    Made migrations for Article model and added few entries with python interactive shell

--- blogapp/articles/models.py
+++ blogapp/articles/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    slug = models.slug()
+    slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     # add author
     # add thumbnail
+    def __str__(self):
+        return self.title
+
