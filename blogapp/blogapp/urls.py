fc272d812ce99d7ce5126deb0b47f716fb920622
Alabhya268 <singhalabhya32@gmail.com>
2020-08-02 03:28:03 +0530

    Added new app accounts ,modified blogapp.urls and blogapp.settings for account app

--- blogapp/blogapp/urls.py
+++ blogapp/blogapp/urls.py
@@ -16,16 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
-from .import views
+from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('about/', views.about),
-    path('',views.homepage),
+    path('', views.homepage),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
