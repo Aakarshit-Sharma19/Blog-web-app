49857d0a75436cc6b61644d9bba28f60d24d34dc
Alabhya268 <singhalabhya32@gmail.com>
2020-08-02 02:46:08 +0530

    Added thumb attribute in article/models for thumbnails,modified article/article_detail.html to show thumbnails, added mediaurl,mediaroot to store thumbnails,made migrations

--- blogapp/articles/models.py
+++ blogapp/articles/models.py
@@ -6,10 +6,12 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+
     # add author
     # add thumbnail
     def __str__(self):
         return self.title
 
     def snippets(self):
-        return self.body[:50]+'...'
+        return self.body[:50] + '...'
--- blogapp/articles/views.py
+++ blogapp/articles/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 
 from .models import Article
 
 
 def article_list(request):
     articles = Article.objects.all().order_by("date")
-    return render(request, 'articles/article_list.html', {'articles': articles})
+    return render(request, "articles/article_list.html", {'articles': articles})
 
 
 def article_detail(request, slug):
-    return HttpResponse(slug)
+    # return HttpResponse(slug)
+    article = Article.objects.get(slug=slug)
+    return render(request, "articles/article_detail.html", {'article': article})
