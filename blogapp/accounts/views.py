2c247f2d2486b0c8bbfb53710cbdb4f6d81434bf
Alabhya268 <singhalabhya32@gmail.com>
2020-08-02 05:49:05 +0530

    Added logout functionality

--- blogapp/accounts/views.py
+++ blogapp/accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
@@ -7,8 +7,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            user=form.save()
-            login(request,user)
+            user = form.save()
+            login(request, user)
             #  log the user in
             return redirect('articles:list')
     else:
@@ -21,8 +21,14 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            login(request,user)
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
