2809e19e051a913a17530c92353da275a3f1214e
Alabhya268 <singhalabhya32@gmail.com>
2020-08-02 19:54:39 +0530

    Added CreateArticle form in frontend

--- blogapp/articles/views.py
+++ blogapp/articles/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . import forms
 
 from .models import Article
 
@@ -16,4 +17,11 @@ def article_detail(request, slug):
 
 @login_required(login_url='/accounts/login/')
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST,request.FILES)
+        if form.is_valid():
+            # save data in db
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form':form})
